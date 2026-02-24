61207ca73bc99ee1de350314cae0e51e67a6ee48
aasmi1811 <asmitaa1811@gmail.com>
2026-02-24 09:18:33 +0000

    feb 24

---
+++ projects/1_monitoring-free-RAM.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+#
+#To monitor free RAM 
+#
+
+FREERAM=$(free -mt | grep Total | awk '{print $4}')
+TH=400
+if [[ $FREERAM -lt $TH ]]
+then
+	echo "WARNING : RAM is running low"
+else
+	echo "RAM is sufficient"
+fi
+
